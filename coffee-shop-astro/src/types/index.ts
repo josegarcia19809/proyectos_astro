d9d02985890879e2c6c84d32c4eb1987cf753a11
Jose Garcia <josegarcia19809@gmail.com>
2026-05-21 14:19:01 -0600

    Coffee Shop Astro: Creando Campos Personalizados en la WP REST API

--- coffee-shop-astro/src/types/index.ts
+++ coffee-shop-astro/src/types/index.ts
@@ -1,5 +1,20 @@
 import {z} from "astro/zod"
 
+const imageSchema = z.object({
+    url: z.string(),
+    width: z.number(),
+    height: z.number()
+})
+
+const featuredImagesSchema = z.object({
+    thumbnail: imageSchema,
+    medium: imageSchema,
+    medium_large: imageSchema,
+    large: imageSchema,
+    full: imageSchema
+})
+
+
 export const BaseWPSchema = z.object({
     id: z.number(),
     title: z.object({
@@ -8,6 +23,7 @@ export const BaseWPSchema = z.object({
     content: z.object({
         rendered: z.string(),
     }),
+    featured_images: featuredImagesSchema,
     acf: z.object({
         subtitle: z.string(),
     }),
