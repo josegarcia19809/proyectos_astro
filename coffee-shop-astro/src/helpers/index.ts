1d2320ade2b1b6645d0ed43d98b7d56a2995ebab
Jose Garcia <josegarcia19809@gmail.com>
2026-05-24 19:22:59 -0600

    Coffee Shop Astro: Creando un Blog en WP REST API y Astro

---
+++ coffee-shop-astro/src/helpers/index.ts
@@ -0,0 +1,20 @@
+export function formatDate(dateStr: string): string {
+    const date = new Date(dateStr)
+    return new Intl.DateTimeFormat('es-ES', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }).format(date)
+}
+
+export function formatAmount(amount: number): string {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(amount)
+}
+
+export function nullToEmptyString(arg: unknown) {
+    return arg ?? ''
+}
--- coffee-shop-astro/src/types/index.ts
+++ coffee-shop-astro/src/types/index.ts
@@ -40,3 +40,22 @@ export const processPageSchema = BaseWPSchema.extend({
         subtitle: z.string(),
     }).catchall(processSchema),
 })
+
+const CategorySchema = z.object({
+    name: z.string(),
+    slug: z.string(),
+})
+
+const CategoriesSchema = z.array(CategorySchema)
+
+export const PostSchema = BaseWPSchema.omit({
+    acf: true
+}).extend({
+    date: z.string(),
+    category_details: CategoriesSchema
+})
+
+export const PostsSchema = z.array(PostSchema)
+
+export type Post = z.infer<typeof PostSchema>
+
